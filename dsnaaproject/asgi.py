237b77725e187c8440e861b94f6838742a112dc5
hmzlz <hamzalazha@esprit.tn>
2021-06-22 13:05:31 +0100

    first commit

---
+++ dsnaaproject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dsnaaproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsnaaproject.settings')
+
+application = get_asgi_application()
